f3f5ebf12d5a557e54936b725b1c2acae8ef572e
Tyron Clark <tyronclark03@github.com>
2025-11-04 19:31:41 -0500

    .ost file filter

--- src/scanner/scanner.cpp
+++ src/scanner/scanner.cpp
@@ -6,26 +6,26 @@ namespace fs = std::filesystem;
 
 void listFiles(const fs::path& dirPath) {
   for (const auto& entry: fs::recursive_directory_iterator(dirPath, fs::directory_options::skip_permission_denied)){
-        
-    std::cout << "Name: " << entry.path().filename() << '\n'
-              << "Path: " << entry.path() << '\n';
+    if (entry.path().extension() == ".ost"){      
+        std::cout << "Name: " << entry.path().filename() << '\n'
+                  << "Path: " << entry.path() << '\n';
 
-  if (fs::is_regular_file(entry.path())){
-    try {
-    std::cout << "Size: " << fs::file_size(entry.path()) << " gb\n\n";
-    } catch (const fs::filesystem_error& e){
-    std::cerr << "Error reading size: " << e.what() << '\n';
-    }
-  } 
+      if (fs::is_regular_file(entry.path())){
+        try {
+        std::cout << "Size: " << fs::file_size(entry.path()) << " bytes\n\n";
+        } catch (const fs::filesystem_error& e){
+        std::cerr << "Error reading size: " << e.what() << '\n';
+        }
+      } 
 
-  else if (fs::is_directory(entry.path())){
-    std::cout << "Type: Directory\n\n";
-  }
+      else if (fs::is_directory(entry.path())){
+        std::cout << "Type: Directory\n\n";
+      }
 
-  else{
-    std::cout <<"Type: Other (symlink, socket, etc.)\n\n";
-  }
-                  
+      else{
+        std::cout <<"Type: Other (symlink, socket, etc.)\n\n";
+      }
+    }                
   }
 }
 
